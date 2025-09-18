054ab1329ed8b6388a1a277fd6851661b9d0461a
Ashutosh Kumar <143290176+Ashutosh-kr7217@users.noreply.github.com>
2025-09-18 05:48:49 +0530

    Create AWS security group for SSH access

---
+++ security-group/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket         = "my-company-terraform-states"          # same bucket
+    key            = "network/vpc/terraform.tfstate"        # path to vpc state
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
+
+resource "aws_security_group" "this" {
+  name        = "allow-ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id  # <-- uses output
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
